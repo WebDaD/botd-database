110cd3023ba99f29b2ae47f84ebd0671522697fc
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-04-16 16:18:47 +0200

    generator start

--- cli.js
+++ cli.js
@@ -1,24 +1,29 @@
 var program = require('commander')
 var moment = require('moment')
-var database = require('lib/database')
+var database = require('./lib/database.js')
+var generator = require('./lib/generator.js')
+var config = require('./config.json')
 
 program
   .version('0.1.0')
-  .command('data [args]', 'database commands')
+
+program
+  .command('database')
+  .alias('data')
   .option('-c, --csv [csv]', 'Import this CSV to blessings.json or export to this file')
   .option('-x, --xml [xml]', 'Import this XML to blessings.json or export to this file')
-  .option('-b, --blessings <blessings>', 'The Blessings-File')
+  .option('-b, --blessings [blessings]', 'The Blessings-File')
   .option('-i, --importmode [importmode]', 'The Mode to import: create or append or append-no-doubles.', /^(append|create|append-no-doubles)$/i)
   .option('-e, --exportmode [exportmode]', 'The Mode to export: xml or csv.', /^(xml|csv)$/i)
-  .option('-m, --mode <mode>', 'Mode of this run. Can be import, export, create', /^(import|create|export)$/i)
+  .option('-m, --mode [mode]', 'Mode of this run. Can be import, export, create', /^(import|create|export)$/i)
   .option('-y, --year [year]', 'For what year.', moment().format('YYYY'))
   .option('-d, --delimeter [delimeter]', 'Delimeter in CSV-File.', ';')
   .option('-n, --newline [newline]', 'Newline in CSV File.', '\n')
   .option('-h, --headers ', 'Header. use if there are or should be header lines')
-  .action(function (args, cmd) {
-    switch (program.mode) {
+  .action(function (args, options) {
+    switch (options.mode) {
       case 'import':
-        database.importCSV(program.csv, program.blessings, program.delimeter, program.newline, program.headers, program.importmode, function (error) {
+        database.importCSV(options.csv, options.blessings, options.delimeter, options.newline, options.headers, options.importmode, function (error) {
           if (error) {
             console.error(error)
           } else {
@@ -27,9 +32,9 @@ program
         })
         break
       case 'export':
-        switch (program.exportmode) {
+        switch (options.exportmode) {
           case 'csv':
-            database.exportCSV(program.csv, program.blessings, program.delimeter, program.newline, program.headers, function (error) {
+            database.exportCSV(options.csv, options.blessings, options.delimeter, options.newline, options.headers, function (error) {
               if (error) {
                 console.error(error)
               } else {
@@ -38,7 +43,7 @@ program
             })
             break
           case 'xml':
-            database.exportXML(program.xml, program.blessings, function (error) {
+            database.exportXML(options.xml, options.blessings, function (error) {
               if (error) {
                 console.error(error)
               } else {
@@ -47,13 +52,13 @@ program
             })
             break
           default:
-            console.error('Unsupported Mode: ' + program.exportmode)
+            console.error('Unsupported Mode: ' + options.exportmode)
             break
         }
 
         break
       case 'create':
-        database.createCalendar(program.year, program.blessings, function (error) {
+        database.createCalendar(options.year, options.blessings, function (error) {
           if (error) {
             console.error(error)
           } else {
@@ -62,10 +67,32 @@ program
         })
         break
       default:
-        console.error('Mode ' + program.mode + ' not avaiable.')
+        console.error('Mode ' + options.mode + ' not avaiable.')
     }
   })
-  .command('generator [args]', 'generator commands')
-  // COPY to dist: blessings.json/csv/xml, feed.php, htaccess
-  // COMPILE LESS, PUG to dist
-  .parse(process.argv)
+program
+  .command('generator')
+  .alias('gen')
+  .option('-f, --folder <folder>', 'Base Folder', '.')
+  .option('-o, --output <output>', 'The output Directoy', 'dist')
+  .option('-y, --year <year>', 'For what year.', moment().format('YYYY'))
+  .action(function (options) {
+    if (options.folder === '.') {
+      options.folder = __dirname
+    }
+    generator.createCSS(options.folder, options.output, function (error, result) {
+      if (error) {
+        console.error(error)
+      } else {
+        generator.copyStatic(options.folder, options.output, config, function (error, result) {
+          if (error) {
+            console.error(error)
+          } else {
+            generator.createPages(options.folder, options.output, options.year, config)
+            console.log('All Done, you may now ftp your stuff.')
+          }
+        })
+      }
+    })
+  })
+program.parse(process.argv)
---
+++ todo.md
@@ -0,0 +1,32 @@
+* google webmaster
+* bing
+* yandex
+* sitemap.xml
+* analytics
+
+* http://yslow.org/
+*   http://schema.org/docs/gs.html
+*   https://github.com/BafS/Gutenberg
+
+*   page-speed insights
++*   facebook-card
++*   twitter-card
++*   share-icons from grace
++*   check favicon
++*   404 page
++*   check if page is aria compatible
++*   check keyword etc
+
+https://developers.google.com/speed/pagespeed/insights/?hl=de&url=http%3A%2F%2Fnew.webdad.eu%2F
++*   https://developers.facebook.com/tools/debug/
+*   https://cards-dev.twitter.com/validator
+*   https://realfavicongenerator.net/favicon_checker
+
+http://wave.webaim.org/report#/feg-ffb.de
++*    https://validator.w3.org/nu/?doc=http%3A%2F%2Ffeg-ffb.de%2F
++*    http://www.evaluera.co.uk/atester
++*   https://search.google.com/structured-data/testing-tool#url=feg-ffb.de
+
+add robots.txt
+
+* facebook page / app_id
