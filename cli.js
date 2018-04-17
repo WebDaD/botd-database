f784b7afedb80b41b1402c1aa0d6f07ef9749eb2
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-04-17 14:39:18 +0200

    Split Commands

--- cli.js
+++ cli.js
@@ -8,71 +8,79 @@ program
   .version('0.1.0')
 
 program
-  .command('database')
-  .alias('data')
-  .option('-c, --csv [csv]', 'Import this CSV to blessings.json or export to this file')
-  .option('-x, --xml [xml]', 'Import this XML to blessings.json or export to this file')
+  .command('import')
+  .alias('i')
+  .option('-c, --csv [csv]', 'Import this CSV to blessings.json')
+  .option('-x, --xml [xml]', 'Import this XML to blessings.json')
   .option('-b, --blessings [blessings]', 'The Blessings-File')
   .option('-i, --importmode [importmode]', 'The Mode to import: create or append or append-no-doubles.', /^(append|create|append-no-doubles)$/i)
-  .option('-e, --exportmode [exportmode]', 'The Mode to export: xml or csv.', /^(xml|csv)$/i)
-  .option('-m, --mode [mode]', 'Mode of this run. Can be import, export, create', /^(import|create|export)$/i)
-  .option('-y, --year [year]', 'For what year.', moment().format('YYYY'))
   .option('-d, --delimeter [delimeter]', 'Delimeter in CSV-File.', ';')
   .option('-n, --newline [newline]', 'Newline in CSV File.', '\n')
   .option('-h, --headers ', 'Header. use if there are or should be header lines')
   .action(function (args, options) {
-    switch (options.mode) {
-      case 'import':
-        database.importCSV(options.csv, options.blessings, options.delimeter, options.newline, options.headers, options.importmode, function (error) {
+    database.importCSV(options.csv, options.blessings, options.delimeter, options.newline, options.headers, options.importmode, function (error) {
+      if (error) {
+        console.error(error)
+      } else {
+        console.log('Import Succesful!')
+      }
+    })
+  })
+
+program
+  .command('export')
+  .alias('e')
+  .option('-c, --csv [csv]', 'Export to this CSV from blessings.json')
+  .option('-x, --xml [xml]', 'Export to this XML from blessings.json')
+  .option('-b, --blessings [blessings]', 'The Blessings-File')
+  .option('-e, --exportmode [exportmode]', 'The Mode to export: xml or csv.', /^(xml|csv)$/i)
+  .option('-d, --delimeter [delimeter]', 'Delimeter in CSV-File.', ';')
+  .option('-n, --newline [newline]', 'Newline in CSV File.', '\n')
+  .option('-h, --headers ', 'Header. use if there are or should be header lines')
+  .action(function (args, options) {
+    switch (options.exportmode) {
+      case 'csv':
+        database.exportCSV(options.csv, options.blessings, options.delimeter, options.newline, options.headers, function (error) {
           if (error) {
             console.error(error)
           } else {
-            console.log('Import Succesful!')
+            console.log('Export Succesful!')
           }
         })
         break
-      case 'export':
-        switch (options.exportmode) {
-          case 'csv':
-            database.exportCSV(options.csv, options.blessings, options.delimeter, options.newline, options.headers, function (error) {
-              if (error) {
-                console.error(error)
-              } else {
-                console.log('Export Succesful!')
-              }
-            })
-            break
-          case 'xml':
-            database.exportXML(options.xml, options.blessings, function (error) {
-              if (error) {
-                console.error(error)
-              } else {
-                console.log('Export Succesful!')
-              }
-            })
-            break
-          default:
-            console.error('Unsupported Mode: ' + options.exportmode)
-            break
-        }
-
-        break
-      case 'create':
-        database.createCalendar(options.year, options.blessings, function (error) {
+      case 'xml':
+        database.exportXML(options.xml, options.blessings, function (error) {
           if (error) {
             console.error(error)
           } else {
-            console.log('Creation Succesful!')
+            console.log('Export Succesful!')
           }
         })
         break
       default:
-        console.error('Mode ' + options.mode + ' not avaiable.')
+        console.error('Unsupported Mode: ' + options.exportmode)
+        break
     }
   })
+
+program
+  .command('calendar')
+  .alias('c')
+  .option('-b, --blessings [blessings]', 'The Blessings-File')
+  .option('-y, --year [year]', 'For what year.', moment().format('YYYY'))
+  .action(function (args, options) {
+    database.createCalendar(options.year, options.blessings, function (error) {
+      if (error) {
+        console.error(error)
+      } else {
+        console.log('Creation Succesful!')
+      }
+    })
+  })
+
 program
   .command('generator')
-  .alias('gen')
+  .alias('g')
   .option('-f, --folder <folder>', 'Base Folder', '.')
   .option('-o, --output <output>', 'The output Directoy', 'dist')
   .option('-y, --year <year>', 'For what year.', moment().format('YYYY'))
