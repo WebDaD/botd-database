7005cb91e06e67f2fceaea572d575a105480465b
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-04-18 11:07:03 +0200

    finished feed

--- assets/php/feed.php
+++ assets/php/feed.php
@@ -1,4 +1,36 @@
 <?php
-// TODO: will have a feed of pages, from start until now.
-
+header('Content-Type: application/xml; charset=utf-8');
+$begin = new DateTime('2018-01-01');
+$now = new DateTime('NOW');
+$interval = DateInterval::createFromDateString('1 day');
+$period = new DatePeriod($begin, $interval, $now);
 ?>
+<rss xmlns:content="http://purl.org/rss/1.0/modules/content/"  xmlns:webfeeds="http://webfeeds.org/rss/1.0" version="2.0">
+  <channel>
+    <title>Blessing of the Day</title>
+    <link>http://botd.webdad.eu</link>
+    <description>Jeden Tag ein neuer Segen.</description>
+    <category>Blessing</category>
+    <language>de-DE</language>
+    <pubDate>Wed, 18 Apr 2018</pubDate>
+    <lastBuildDate><?php echo $now->format("D, d M Y H:i:s O");?></lastBuildDate>
+    <image>
+      <url>https://botd.webdad.eu/images/android-chrome-512x512.png</url>
+      <title>Blessing of the Day</title>
+      <link>http://botd.webdad.eu</link>
+    </image>
+    <webfeeds:cover image="https://botd.webdad.eu/images/android-chrome-512x512.png" />
+    <webfeeds:icon>https://botd.webdad.eu/images/safari-pinned-tab.svg</webfeeds:icon>
+    <webfeeds:logo>https://botd.webdad.eu/images/safari-pinned-tab.svg</webfeeds:logo>
+    <webfeeds:accentColor>4CAF50</webfeeds:accentColor>
+    <webfeeds:related layout="card" target="browser"/>
+    <?php foreach ($period as $dt): ?>
+      <item>
+        <title>Segen am <?php echo $dt->format("d.m.Y");?></title>
+        <link>http://botd.webdad.eu/ <?php echo $dt->format("Ymd");?>.html</link>
+        <description>Der Segen für den <?php echo $dt->format("d.m.Y");?></description>
+        <pubDate><?php echo $dt->format("D, d M Y H:i:s O");?></pubDate>
+      </item>
+    <?php endforeach; ?>
+  </channel>
+</rss>
